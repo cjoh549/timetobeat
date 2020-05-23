a6c23b43de74f35ddd0da95c55fc9a3a1e520568
Cliff Johnson <cliff.johnson@gmail.com>
2020-05-23 16:05:24 -0700

    Added additional tests

--- timetobeat_test.py
+++ timetobeat_test.py
@@ -11,6 +11,22 @@ def test_bad_game_title():
     result = search_hltb(game_title)
     assert result == False
 
-def test_bad_request_uri():
-    uri = "httq://www.test.com"
-    result = make_request(uri)
+def test_good_game_url():
+    game_url = "game?id=5698"
+    result = get_time_to_beat(game_url)
+    assert isinstance(result, dict) == True
+
+def test_bad_game_url():
+    game_url = "game?id=abc"
+    result = get_time_to_beat(game_url)
+    assert result == False
+
+def test_multiplayer():
+    game_url = "game?id=29358"
+    result = get_time_to_beat(game_url)
+    assert result['Co-Op'] == "26"
+
+def test_single():
+    game_url = "game?id=5698"
+    result = get_time_to_beat(game_url)
+    assert result['Main Story'] == "17"
