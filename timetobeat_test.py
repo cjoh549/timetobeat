344a11d7f6095282e62186c8727b1aa725737999
Cliff Johnson <cjoh549@wgu.edu>
2020-05-23 14:50:15 -0700

    Create timetobeat_test.py

---
+++ timetobeat_test.py
@@ -0,0 +1,16 @@
+import pytest
+from timetobeat import *
+
+def test_good_game_title():
+    game_title = "Mass Effect"
+    result = search_hltb(game_title)
+    assert result == "game?id=5698"
+
+def test_bad_game_title():
+    game_title = "ASDKJasdfasd asdfaena;sdf $"
+    result = search_hltb(game_title)
+    assert result == False
+
+def test_bad_request_uri():
+    uri = "httq://www.test.com"
+    result = make_request(uri)
