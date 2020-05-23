23b3b493058dc79c8994bcb5bf6cc61949b8e718
Cliff Johnson <cliff.johnson@gmail.com>
2020-05-23 16:56:58 -0700

    Added tests for fuzzy matching

--- timetobeat_test.py
+++ timetobeat_test.py
@@ -11,6 +11,16 @@ def test_bad_game_title():
     result = search_hltb(game_title)
     assert result == False
 
+def test_default_fuzzy_search():
+    game_title = "Halo Combat Evolved" #action title Halo: Combat Evolved
+    result = search_hltb(game_title)
+    assert result == "game?id=4269"
+
+def test_extra_fuzzy_search():
+    game_title = "Halo"
+    result = search_hltb(game_title, .80)
+    assert result == "game?id=4263"
+
 def test_good_game_url():
     game_url = "game?id=5698"
     result = get_time_to_beat(game_url)
